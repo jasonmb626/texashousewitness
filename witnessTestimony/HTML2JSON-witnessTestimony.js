6ac1ff3eb311f499255c5209df7e8b0a31fab658
Jason Brunelle <jason@jasonbrunelle.com>
2021-03-04 07:06:16 -0600

    a -> c strategy. Add new a strategy and text on one record. Success

--- witnessTestimony/HTML2JSON-witnessTestimony.js
+++ witnessTestimony/HTML2JSON-witnessTestimony.js
@@ -1,18 +1,41 @@
 const fs = require('fs');
 const path = require('path');
 
-const strategyA = require(path.join('strategies', 'a.js'));
+console.log(__dirname);
 
-const inFileName = process.argv[2];
+const strategyA = require('./strategies/a');
+// const strategyB = require('./strategies/b');
+const strategyC = require('./strategies/c');
+//get a list of input files from the commandline
+//find . -type f
+
+let searchStr = './capitol.texas.gov/tlodocs/';
+let startIndex = process.argv[2].indexOf(searchStr) + searchStr.length;
+let endIndex = process.argv[2].indexOf('/', startIndex);
+let session = process.argv[2].substring(startIndex, endIndex);
+let leg = session.substring(0, 2);
+
+const inFileName = path.basename(process.argv[2]);
+
 const outFileName = `${inFileName.replace('.HTM', '.json')}`;
 
 const html = fs
-  .readFileSync(path.join('witnessTestimony', 'HTML', inFileName))
+  .readFileSync(path.join('witnessTestimony', 'HTML', process.argv[2]))
   .toString();
 
-const records = [];
+const meeting_cd = inFileName.replace('.HTM', '');
+let scrape;
 
-strategyA.scrape(html);
+switch (parseInt(leg)) {
+  case 75:
+    scrape = strategyA.scrape;
+    break;
+  case 86:
+    scrape = strategyC.scrape;
+    break;
+}
+
+const records = scrape(html, meeting_cd);
 
 fs.writeFileSync(
   path.join('witnessTestimony', 'JSON', outFileName),
--- witnessTestimony/strategies/a.js
+++ witnessTestimony/strategies/a.js
@@ -1,139 +1,185 @@
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-
 module.exports = {
-  scrape: function (html) {
-    const dom = new JSDOM(html).window.document;
-    let page = 1;
-
-    let searchStr;
-    let startIndex = -1;
-    let endIndex = -1;
-
-    let bill_name = '';
-    let bill_cd = '';
-    let session = '';
-    let position = '';
-    let self = false;
-    let rbnt = false;
+  scrape: function (html, meeting_cd) {
+    const witnessRecords = [];
+    let witnessRecord = {};
+    let mode = 'ENTER';
     let committee = '';
     let dttm = '';
+    let startIndex = -1;
+    let endIndex = -1;
+    let searchStr = '';
+    let session = '';
+    let bill_cd = '';
+    let bill_name = '';
+    let witnessName = '';
+    let position = '';
+    let rbnt = false;
+    let self = false;
+    let restOfLine = '';
 
-    const Ps = dom.getElementsByTagName('p');
-    Array.from(Ps).forEach((p, i) => {
-      const sp = p.firstChild;
-      if (i === 0) {
-        // do nothing
-      } else if (i === 1) {
-        committee = sp.textContent
-          .replace(new RegExp(String.fromCharCode(65533), 'g'), '')
-          .replace('\n', ' ')
-          .trim();
-      } else if (i === 2) {
-        dttm = sp.textContent
-          .replace(new RegExp(String.fromCharCode(65533), 'g'), '')
-          .replace('\n', ' ')
-          .trim();
-      } else {
-        if (sp.innerHTML.includes('/BillLookup/History.aspx?')) {
-          const a = sp.getElementsByTagName('a')[0];
-          const href = a.getAttribute('href');
-
-          searchStr = 'LegSess=';
-          startIndex = href.indexOf(searchStr) + searchStr.length;
-          endIndex = href.indexOf('&', startIndex);
-          session = href.substring(startIndex, endIndex);
-          searchStr = 'Bill=';
-          startIndex = href.indexOf(searchStr, startIndex) + searchStr.length;
-          endIndex = href.length;
-          bill_cd = href.substring(startIndex, endIndex);
-          bill_name = a.firstChild.textContent;
+    const lines = html.split('\n');
+    lines.forEach((line) => {
+      restOfLine = '';
+      if (mode === 'ENTER' && line.includes('WITNESS LIST')) {
+        mode = 'COMMITTEE NAME';
+      } else if (mode === 'COMMITTEE NAME' && line.trim() !== '') {
+        committee = line.trim();
+        mode = 'DTTM';
+      } else if (mode === 'DTTM' && line.trim() !== '') {
+        dttm = line.trim();
+        mode = 'WITNESS_RECORDS';
+      } else if (mode === 'WITNESS_RECORDS' && !lineBlankOrClosingHTML(line)) {
+        searchStr = '/BillLookup/History.aspx?LegSess=';
+        if (line.includes(searchStr)) {
+          witnessRecord = {};
+          witnessName = '';
+          organization = '';
           position = '';
-          witness = '';
           rbnt = false;
-        } else if (sp.textContent.includes('For:')) {
-          position = 'For';
-        } else if (sp.textContent.includes('On:')) {
-          position = 'On';
-        } else if (sp.textContent.includes('Against:')) {
-          position = 'Against';
-        } else if (
-          sp.innerHTML.includes('Registering,') &&
-          sp.innerHTML.includes('but not testifying:')
-        ) {
-          rbnt = true;
-        } else if (
-          sp.innerHTML.includes('<u>WITNESS') &&
-          sp.innerHTML.includes('LIST</u>')
-        ) {
-          //do nothing
+          self = false;
+          startIndex = line.indexOf(searchStr) + searchStr.length;
+          endIndex = line.indexOf('&', startIndex);
+          session = line.substring(startIndex, endIndex);
+          searchStr = 'Bill=';
+          startIndex = line.indexOf(searchStr) + searchStr.length;
+          endIndex = line.indexOf('"', startIndex);
+          bill_cd = line.substring(startIndex, endIndex);
+          startIndex = line.indexOf('>', endIndex) + 1;
+          endIndex = line.indexOf('</A>', startIndex);
+          bill_name = line.substring(startIndex, endIndex);
         } else {
-          const fullWitnessName = sp.textContent
-            .replace(new RegExp(String.fromCharCode(65533), 'g'), '')
-            .trim();
-          if (fullWitnessName == page) {
-            //this is actually a line in the HTML showing what page of records it is.
-            page++;
+          searchStr = 'For:';
+          if (line.includes(searchStr)) {
+            position = 'For';
+            restOfLine = line.replace(searchStr, '').trim();
+          }
+          searchStr = 'On:';
+          if (line.includes(searchStr)) {
+            position = 'On';
+            restOfLine = line.replace(searchStr, '').trim();
+          }
+          searchStr = 'Against:';
+          if (line.includes(searchStr)) {
+            position = 'Against';
+            restOfLine = line.replace(searchStr, '').trim();
+          }
+          searchStr = 'Registering, but not testifying:';
+          if (line.includes(searchStr)) {
+            position = '';
+            rbnt = true;
           } else {
+            if (restOfLine === '') restOfLine = line.trim();
             self = false;
-            const splitWitness = splitFullWitness(fullWitnessName);
-            if (
-              fullWitnessName.toUpperCase().includes('SELF;') ||
-              fullWitnessName.toUpperCase().includes('SELF)')
-            )
+            const splitWitness = splitOneWitness(restOfLine);
+            if (splitWitness.organization.toUpperCase().startsWith('SELF')) {
               self = true;
-            if (session != '' && bill_cd != '') {
-              records.push({
-                meeting_cd: inFileName.replace('.HTM', ''),
-                committee,
-                dttm,
-                session,
-                bill_name,
-                bill_cd,
-                position,
-                rbnt,
-                self,
-                fullWitnessName,
-                given_name: splitWitness.givenName,
-                sur_name: splitWitness.surName,
-                organization: splitWitness.organization,
-              });
+              splitWitness.organization = splitWitness.organization
+                .substring(4, splitWitness.organization.length)
+                .trim();
+            } else if (
+              splitWitness.organization.toUpperCase().endsWith('SELF')
+            ) {
+              self = true;
+              splitWitness.organization = splitWitness.organization
+                .substring(
+                  splitWitness.organization.length - 4,
+                  splitWitness.organization.length
+                )
+                .trim();
             }
+            searchStr = 'SELF &';
+            if (splitWitness.organization.toUpperCase().includes(searchStr)) {
+              startIndex = splitWitness.organization
+                .toUpperCase()
+                .indexOf(searchStr);
+              endIndex = startIndex + searchStr.length;
+              const middleOfString = splitWitness.organization.substring(
+                startIndex,
+                endIndex
+              );
+              splitWitness.organization = splitWitness.organization.replace(
+                middleOfString,
+                ''
+              );
+              self = true;
+            }
+            searchStr = '& SELF';
+            if (splitWitness.organization.toUpperCase().includes(searchStr)) {
+              startIndex = splitWitness.organization
+                .toUpperCase()
+                .indexOf(searchStr);
+              endIndex = startIndex + searchStr.length;
+              const middleOfString = splitWitness.organization.substring(
+                startIndex,
+                endIndex
+              );
+              splitWitness.organization = splitWitness.organization.replace(
+                middleOfString,
+                ''
+              );
+              self = true;
+            }
+            witnessRecord = {
+              meeting_cd,
+              committee,
+              dttm,
+              session,
+              bill_name,
+              bill_cd,
+              position,
+              rbnt,
+              self,
+              fullWitnessName: restOfLine,
+              given_name: splitWitness.given_name,
+              sur_name: splitWitness.sur_name,
+              organization: splitWitness.organization,
+            };
+            witnessRecords.push(witnessRecord);
           }
         }
       }
     });
-  },
-  splitFullWitness: function (fullWitness) {
-    let witnessObj = {};
-    const splitWitness = fullWitness.split('\n');
-    witnessObj.surName = splitWitness[0].trim();
-    if (witnessObj.surName.endsWith(',')) {
-      witnessObj.surName = witnessObj.surName.substring(
-        0,
-        witnessObj.surName.length - 1
-      );
-    }
-    if (splitWitness[1]) {
-      const givenAndOrgName = splitWitness[1].split('(');
-      witnessObj.givenName = givenAndOrgName[0].trim();
-      if (givenAndOrgName[1]) {
-        witnessObj.organization = givenAndOrgName[1]
-          .replace('Self;', '')
-          .replace('Self)', '')
-          .trim();
-        if (witnessObj.organization.endsWith(')')) {
-          witnessObj.organization = witnessObj.organization.substring(
-            0,
-            witnessObj.organization.length - 1
-          );
-        }
-      } else {
-        //Most likely scenario: Organization name listed as individual's name
-        witnessObj.organization =
-          witnessObj.givenName + ' ' + witnessObj.surName;
-      }
-    }
-    return witnessObj;
+    return witnessRecords;
   },
 };
+
+function splitOneWitness(fullWitnessName) {
+  let given_name = '';
+  let sur_name = '';
+  let organization = '';
+  let restOfLine = '';
+
+  let startIndex = fullWitnessName.indexOf('(');
+  let endIndex = -1;
+  if (startIndex > 1) {
+    endIndex = fullWitnessName.indexOf(')', startIndex) + 1;
+    if (endIndex === -1) endIndex = fullWitnessName.length;
+    organization = fullWitnessName.substring(startIndex, endIndex);
+  }
+  restOfLine = fullWitnessName.replace(organization, '').trim();
+  organization = organization.substring(1, organization.length - 1);
+  startIndex = 0;
+  endIndex = restOfLine.indexOf(',');
+  if (endIndex !== -1) {
+    sur_name = restOfLine.substring(0, endIndex);
+    startIndex = endIndex + 1;
+    endIndex = restOfLine.length;
+    given_name = restOfLine.substring(startIndex, endIndex).trim();
+  } else {
+    given_name = restOfLine.substring(0, restOfLine.length).trim();
+  }
+  return {
+    given_name,
+    sur_name,
+    organization,
+  };
+}
+
+function lineBlankOrClosingHTML(line) {
+  let blank = false;
+  if (line.trim() === '') blank = true;
+  if (line.toUpperCase().includes('</PRE>')) blank = true;
+  if (line.toUpperCase().includes('</BODY>')) blank = true;
+  if (line.toUpperCase().includes('</HTML>')) blank = true;
+  return blank;
+}
